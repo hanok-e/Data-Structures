8b237101cb496de371f9d2c967e5533b82f5206c
svfmb <hanokendigeri@gmail.com>
2022-08-17 20:06:14 +0530

    Adding code for bst problems

--- week4_binary_search_trees/1_tree_traversals/tree-orders.cpp
+++ week4_binary_search_trees/1_tree_traversals/tree-orders.cpp
@@ -1,82 +1,160 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <bits/stdc++.h>
 #if defined(__unix__) || defined(__APPLE__)
 #include <sys/resource.h>
 #endif
 
 using std::vector;
+using std::queue;
+using std::pair;
+using std::unordered_map;
 using std::ios_base;
 using std::cin;
 using std::cout;
 
 class TreeOrders {
-  int n;
-  vector <int> key;
-  vector <int> left;
-  vector <int> right;
+    int n;
+    vector <int> key;
+    vector <int> left;
+    vector <int> right;
+
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
 
 public:
-  void read() {
-    cin >> n;
-    key.resize(n);
-    left.resize(n);
-    right.resize(n);
-    for (int i = 0; i < n; i++) {
-      cin >> key[i] >> left[i] >> right[i];
+    void read() {
+        cin >> n;
+        key.resize(n);
+        left.resize(n);
+        right.resize(n);
+        for (int i = 0; i < n; i++) {
+            cin >> key[i] >> left[i] >> right[i];
+        }
     }
-  }
+
+    TreeNode* build_tree() {
+        if (key.size() == 0) {
+            return nullptr;
+        }
+        TreeNode* root = new TreeNode(key[0]);
+        queue<TreeNode*> q;
+        q.push(root);
+        unordered_map<int, pair<int, int>> mp;
+        for (int i = 0; i < key.size(); i++) {
+            mp[key[i]] = {left[i], right[i]};
+        }
+        while (!q.empty()) {
+            TreeNode* parent = q.front();
+            q.pop();
+            if(mp[parent->val].first == -1) {
+                parent->left = nullptr;
+            } else {
+                TreeNode* left = new TreeNode(key[mp[parent->val].first]);
+                parent->left = left;
+                q.push(left);
+            }
+            if (mp[parent->val].second == -1) {
+                parent->right = nullptr;
+            } else {
+                TreeNode* right = new TreeNode(key[mp[parent->val].second]);
+                parent->right = right;
+                q.push(right);
+            }
+        }
+        return root;
+    }
+
+    void in_order_helper(TreeNode* root, vector<int>& result) {
+        if (root == nullptr) {
+            return;
+        }
+        in_order_helper(root->left, result);
+        result.push_back(root->val);
+        in_order_helper(root->right, result);
+    }
+
+    void pre_order_helper(TreeNode* root, vector<int>& result) {
+        if (root == nullptr) {
+            return;
+        }
+        result.push_back(root->val);
+        pre_order_helper(root->left, result);
+        pre_order_helper(root->right, result);
+    }
+
+    void post_order_helper(TreeNode* root, vector<int>& result) {
+        if (root == nullptr) {
+            return;
+        }
+        post_order_helper(root->left, result);
+        post_order_helper(root->right, result);
+        result.push_back(root->val);
+    }
+
+    vector <int> in_order() {
+        vector<int> result;
+        // Finish the implementation
+        // You may need to add a new recursive method to do that
+
+        TreeNode* root = build_tree();
+        in_order_helper(root, result);
+        return result;
+    }
+
+    vector <int> pre_order() {
+        vector<int> result;
+        // Finish the implementation
+        // You may need to add a new recursive method to do that
 
 
-  vector <int> in_order() {
-    vector<int> result;
-    // Finish the implementation
-    // You may need to add a new recursive method to do that
+        TreeNode* root = build_tree();
+        pre_order_helper(root, result);
+        return result;
+    }
 
-    return result;
-  }
+    vector <int> post_order() {
+        vector<int> result;
+        // Finish the implementation
+        // You may need to add a new recursive method to do that
 
-  vector <int> pre_order() {
-    vector<int> result;    
-    // Finish the implementation
-    // You may need to add a new recursive method to do that
-    
-    return result;
-  }
-
-  vector <int> post_order() {
-    vector<int> result;
-    // Finish the implementation
-    // You may need to add a new recursive method to do that
-    
-    return result;
-  }
+        TreeNode* root = build_tree();
+        post_order_helper(root, result);
+        return result;
+    }
 };
 
 void print(vector <int> a) {
-  for (size_t i = 0; i < a.size(); i++) {
-    if (i > 0) {
-      cout << ' ';
+    for (size_t i = 0; i < a.size(); i++) {
+        if (i > 0) {
+            cout << ' ';
+        }
+        cout << a[i];
     }
-    cout << a[i];
-  }
-  cout << '\n';
+    cout << '\n';
 }
 
 int main_with_large_stack_space() {
-  ios_base::sync_with_stdio(0);
-  TreeOrders t;
-  t.read();
-  print(t.in_order());
-  print(t.pre_order());
-  print(t.post_order());
-  return 0;
+    ios_base::sync_with_stdio(0);
+    TreeOrders t;
+    t.read();
+    print(t.in_order());
+    print(t.pre_order());
+    print(t.post_order());
+    return 0;
 }
 
 int main (int argc, char **argv)
 {
 #if defined(__unix__) || defined(__APPLE__)
-  // Allow larger stack space
+    // Allow larger stack space
   const rlim_t kStackSize = 16 * 1024 * 1024;   // min stack size = 16 MB
   struct rlimit rl;
   int result;
@@ -96,6 +174,6 @@ int main (int argc, char **argv)
   }
 #endif
 
-  return main_with_large_stack_space();
+    return main_with_large_stack_space();
 }
 
--- week4_binary_search_trees/2_is_bst/is_bst.cpp
+++ week4_binary_search_trees/2_is_bst/is_bst.cpp
@@ -1,39 +1,105 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <bits/stdc++.h>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
+using std::unordered_map;
+using std::queue;
+using std::pair;
 
 struct Node {
-  int key;
-  int left;
-  int right;
+    int key;
+    int left;
+    int right;
 
-  Node() : key(0), left(-1), right(-1) {}
-  Node(int key_, int left_, int right_) : key(key_), left(left_), right(right_) {}
+    Node() : key(0), left(-1), right(-1) {}
+    Node(int key_, int left_, int right_) : key(key_), left(left_), right(right_) {}
 };
 
-bool IsBinarySearchTree(const vector<Node>& tree) {
-  // Implement correct algorithm here
-  return true;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode* build_tree(const vector<Node>& tree) {
+    vector<int> key(tree.size()); vector<int> left(tree.size()); vector<int> right(tree.size());
+    if (key.size() == 0) {
+        return nullptr;
+    }
+    for (int i = 0; i < tree.size(); i++) {
+        key[i] = tree[i].key;
+        left[i] = tree[i].left;
+        right[i] = tree[i].right;
+    }
+    TreeNode* root = new TreeNode(key[0]);
+    queue<TreeNode*> q;
+    q.push(root);
+    unordered_map<int, pair<int, int>> mp;
+    for (int i = 0; i < key.size(); i++) {
+        mp[key[i]] = {left[i], right[i]};
+    }
+    while (!q.empty()) {
+        TreeNode* parent = q.front();
+        q.pop();
+        if(mp[parent->val].first == -1) {
+            parent->left = nullptr;
+        } else {
+            TreeNode* left = new TreeNode(key[mp[parent->val].first]);
+            parent->left = left;
+            q.push(left);
+        }
+        if (mp[parent->val].second == -1) {
+            parent->right = nullptr;
+        } else {
+            TreeNode* right = new TreeNode(key[mp[parent->val].second]);
+            parent->right = right;
+            q.push(right);
+        }
+    }
+    return root;
 }
 
+bool IsBinarySearchTreeHelper(TreeNode* curr_node, long long low, long long high) {
+    if (curr_node == nullptr) {
+        return true;
+    }
+    if (curr_node->val <= low || curr_node->val >= high) {
+        return false;
+    }
+    return IsBinarySearchTreeHelper(curr_node->left, low, curr_node->val) &&
+    IsBinarySearchTreeHelper(curr_node->right, curr_node->val, high);
+}
+
+
+bool IsBinarySearchTree(const vector<Node>& tree) {
+    // Implement correct algorithm here
+    TreeNode* root = build_tree(tree);
+    return IsBinarySearchTreeHelper(root, LONG_MIN, LONG_MAX);
+}
+
+
+
 int main() {
-  int nodes;
-  cin >> nodes;
-  vector<Node> tree;
-  for (int i = 0; i < nodes; ++i) {
-    int key, left, right;
-    cin >> key >> left >> right;
-    tree.push_back(Node(key, left, right));
-  }
-  if (IsBinarySearchTree(tree) {
-    cout << "CORRECT" << endl;
-  } else {
-    cout << "INCORRECT" << endl;
-  }
-  return 0;
+    int nodes;
+    cin >> nodes;
+    vector<Node> tree;
+    for (int i = 0; i < nodes; ++i) {
+        int key, left, right;
+        cin >> key >> left >> right;
+        tree.push_back(Node(key, left, right));
+    }
+    if (IsBinarySearchTree(tree)) {
+        cout << "CORRECT" << endl;
+    } else {
+        cout << "INCORRECT" << endl;
+    }
+    return 0;
 }
